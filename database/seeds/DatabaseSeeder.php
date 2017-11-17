88ea2504f43b0a468022f3e37591e04803a52685
ali mathofani <alimathofani@gmail.com>
2017-11-17 22:07:18 +0700

    Calling All Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(BranchesSeeder::class);
+        $this->call([
+        	CategoryProductSeeder::class,
+        	CategorySeeder::class
+        	
+        ]);
+
     }
 }
