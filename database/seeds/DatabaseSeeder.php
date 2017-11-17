63bf3e2f0c9865493f5bd31877cb33056c7c429d
ali mathofani <alimathofani@gmail.com>
2017-11-17 23:51:21 +0700

    Calling all seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
         	CategoryProductSeeder::class,
-        	CategorySeeder::class
-        	
+            CategorySeeder::class
+        	UsersSeeder::class
         ]);
 
     }
